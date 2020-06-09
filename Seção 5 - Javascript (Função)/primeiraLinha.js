897a770613b4b0883cc30c74a6439bb86d9b59e3
brunoamaia <brunoamaia94@gmail.com>
2020-06-09 15:44:27 -0300

    Create primeiraLinha.js

---
+++ Seção 5 - Javascript (Função)/primeiraLinha.js
@@ -0,0 +1,34 @@
+// Função em JS é First-Class Object (Citizens)
+//Higher-order function
+
+// Criar de forma literal
+function fun1() {}
+
+// Armazenar uma variável
+const fun2 = function () {}
+
+// Armazenar em um array
+const array = [function (a, b) {return a+b}, fun1, fun2]
+console.log(array[0](2, 3));
+
+// Armazenar em um atributo de Objeto
+const obj = {}
+obj.falar = function () { return 'Opa'}
+console.log(obj.falar());
+
+// Passar uma função como parametro
+function run(fun) {
+    fun()
+} // Chamada
+run(function() {console.log('Executando...') })
+
+// Uma função pode retornar/coner uma função
+function soma (a, b) {
+    return function (c) {
+        console.log(a+b+c)
+    }
+} //chamadas
+soma(2,3)(4)    //ou
+
+const cincoMais = soma(2, 3)
+cincoMais(4)
