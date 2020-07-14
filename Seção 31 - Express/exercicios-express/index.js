c7d50bbc344c438fd07a9d2b13a2979d683e856e
brunoamaia <brunoamaia94@gmail.com>
2020-07-14 17:51:41 -0300

    Métodos de acesso http

--- Seção 31 - Express/exercicios-express/index.js
+++ Seção 31 - Express/exercicios-express/index.js
@@ -1,6 +1,21 @@
 const express = require('express')
 const app = express()
 
+// Ele para na primeira "chamada" q for aceito
+// Se a primeira for a mais genérica (igual a abaixo), nunca acesssará as demais
+/*app.use((req, res) => {     //Toda vez que qualquer subdominio é acessado, ele vai ser chamado. Independentemente do método/requisição utilizado
+    res.send('Acessou!!!')
+})*/
+
+app.use('/hwe', (req, res) => {     // Subdominio especifico
+    res.send('Página hwe 🤖')
+})
+
+app.get('/get', (req, res) => {             // Requisição do tipo get
+    res.send('get 👉')
+})
+
+
 app.listen(3000, () => {
     console.log('Backend executando ...')
 })
