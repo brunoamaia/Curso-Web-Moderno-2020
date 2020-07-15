6315c883c2bd1420473e3640e3e5943d0b1797bd
brunoamaia <brunoamaia94@gmail.com>
2020-07-15 14:08:26 -0300

    Update saudacaoMid.js

--- Seção 31 - Express/exercicios-express/saudacaoMid.js
+++ Seção 31 - Express/exercicios-express/saudacaoMid.js
@@ -1,5 +1,5 @@
 function saudacao(nome) {   // Exemplo de função Midlleware
-    return function(req, res, next) {
+    return function(req, res, next) {   // Coloca dentro desta função para que seja executado toda vez que for requisitada
         console.log(`${nome}, seja bem vindo!!!`);
         next()
     }
