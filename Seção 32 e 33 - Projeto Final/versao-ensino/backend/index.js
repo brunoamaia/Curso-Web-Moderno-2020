6ba1a55251bdc5b710d2073747900ec68d6735ab
brunoamaia <brunoamaia94@gmail.com>
2020-07-23 11:41:51 -0300

    Update index.js

--- Seção 32 e 33 - Projeto Final/versao-ensino/backend/index.js
+++ Seção 32 e 33 - Projeto Final/versao-ensino/backend/index.js
@@ -1,5 +1,5 @@
 const app = require('express')()    // Faz duas chamas consecutivas. O resultado da chamada do "express", será armazenado em  "app"
 
 app.listen(3000, () => {
-    console.log('Backend executando2 ...')
+    console.log('Backend executando ...')
 })
