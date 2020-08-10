c77114084151770a59b59b4f06cce1d98a3d4a08
brunoamaia <brunoamaia94@gmail.com>
2020-08-10 15:21:45 -0300

    Configurar conexão com o BD e criar as migrations para as tabelas iniciais

--- Seção 32 e 33 - Projeto Final/versao-ensino/backend/index.js
+++ Seção 32 e 33 - Projeto Final/versao-ensino/backend/index.js
@@ -1,5 +1,8 @@
 const app = require('express')()    // Faz duas chamas consecutivas. O resultado da chamada do "express", será armazenado em  "app"
 const consign = require('consign')  // O consign "injeta" o "app" nos middlewares, tornando mais fácil administrar as dependencias
+const db = require('./config/db')   // Comunicação/configuração com o Banco de Dados
+
+app.db = db                         // "Injeta" as funcionalidades do "db" no comando app.db
 
 consign()
     .then('./config/middlewares.js')    // Chama os midllewares (Trabalha com o JSON)
