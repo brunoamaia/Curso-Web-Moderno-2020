d5cdde34b5b52146460a2e32b3553abd39870141
brunoamaia <brunoamaia94@gmail.com>
2020-08-11 14:57:34 -0300

    Remover categorisa

--- Seção 32 e 33 - Projeto Final/versao-ensino/backend/api/category.js
+++ Seção 32 e 33 - Projeto Final/versao-ensino/backend/api/category.js
@@ -26,4 +26,33 @@ module.exports = app => {
         .catch(err => res.status(500).send(err))
     }
   }
+
+  // Remover categorias
+  const remove = async (req, res) => {
+    try {   // Validar o ID da categoria
+      existOrError(req.params.id, 'Código da categoria não foi informado.')
+
+      // Verificar se existem subcategorias da categoria informada/selecionada
+      const subcategory = await app.db('categories')
+        .where({ parentId: req.params.id })
+      notExistOrError(subcategory, 'Categoria possui subcategorias.')
+
+      // Verificar se existem artigos associados a categoria
+      const articles = await app.db('articles')
+        .where({ categoryId: req.params.id })
+      notExistOrError(articles, 'Categoria possui artigos associados.')
+
+      // Deletar a categoria
+      const rowDeleted = await app.db('categories')
+        .where({ id: req.params.id }).del()
+      existOrError(rowDeleted, 'Categoria não foi encontrada.')
+
+      res.status(204).send()
+
+    } catch (msg) {
+      res.status(400).send(msg)
+    }
+  }
+
+  
 }
