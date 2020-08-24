2f190e493a1b1cf595a67db61f641330d6fe4ef7
brunoamaia <brunoamaia94@gmail.com>
2020-08-24 16:17:47 -0300

    Update mongodb.js

--- Seção 32 e 33 - Projeto Final/versao-ensino/backend/config/mongodb.js
+++ Seção 32 e 33 - Projeto Final/versao-ensino/backend/config/mongodb.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
-const { mongoDb } = require('../.env')
+const { mongoDb } = require('../.env')  // rota para o MongoDB
 
-mongoose.connect(mongoDb, {useNewUrlParser: true})  // rota para o MongoDB
+mongoose.connect(mongoDb, {useNewUrlParser: true}) 
   .catch(e => {
     const msg = 'ERRO Não foi possível conectar com o MongoDB!'
     console.log('\x1b[41m%s\x1b[37m', msg, '\x1b[0m')
