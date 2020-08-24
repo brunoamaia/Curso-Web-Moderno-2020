8e3d32b1647545a93451192fe1eaa99185227cf5
brunoamaia <brunoamaia94@gmail.com>
2020-08-24 10:39:15 -0300

    Validando Usuário Administrador

--- Seção 32 e 33 - Projeto Final/versao-ensino/backend/config/routes.js
+++ Seção 32 e 33 - Projeto Final/versao-ensino/backend/config/routes.js
@@ -1,4 +1,6 @@
 // "app" é importado pelo consign no Index, por isso não é importado aqui
+const admin = require('./admin')
+
 module.exports = app => {
 	app.post('/signup', app.api.user.save)
 	app.post('/signin', app.api.auth.signin)								// Autenticar login
@@ -6,18 +8,18 @@ module.exports = app => {
 
 	app.route('/users')
 		.all(app.config.passport.authenticate())	// Só permite acessar os métodos, após autenticar
-  	.get(app.api.user.get)      // Lista todos os usuários no BD
-		.post(app.api.user.save)    // Inserir usuário >> >>  Forma de "chamar" como o [consign]: endereço do arquivo.método utilizado
+  	.get(admin(app.api.user.get))      // Lista todos os usuários no BD
+		.post(admin(app.api.user.save))  // Inserir usuário >> >>  Forma de "chamar" como o [consign]: endereço do arquivo.método utilizado
 
   app.route('/users/:id')
 		.all(app.config.passport.authenticate())
-		.get(app.api.user.getById)  // Selecionar usuário por ID
-		.put(app.api.user.save)     // Atualizar informações do usuário
+		.get(admin(app.api.user.getById))  // Selecionar usuário por ID
+		.put(admin(app.api.user.save))     // Atualizar informações do usuário
 
   app.route('/categories')
 		.all(app.config.passport.authenticate())
-		.get(app.api.category.get)		// Listar todas as categorias 
-		.post(app.api.category.save)	// Inserir categoria 
+		.get(admin(app.api.category.get))		// Listar todas as categorias 
+		.post(admin(app.api.category.save))	// Inserir categoria 
 
 	// A ordem é importante. A função deve vir antes do parâmetro
 	app.route('/categories/tree')
@@ -26,20 +28,20 @@ module.exports = app => {
 
   app.route('/categories/:id')
 		.all(app.config.passport.authenticate())
-		.delete(app.api.category.remove)  // Deletar usuário por ID
-		.get(app.api.category.getById)    // Selecionar usuário por ID
-		.put(app.api.category.save)       // Atualizar informações do usuário
+		.delete(admin(app.api.category.remove)) // Deletar usuário por ID
+		.get(app.api.category.getById)    			// Selecionar usuário por ID
+		.put(admin(app.api.category.save))      // Atualizar informações do usuário
 
 	app.route('/articles')
 		.all(app.config.passport.authenticate())
-		.get(app.api.article.get)
-		.post(app.api.article.save)
+		.get(admin(app.api.article.get))
+		.post(admin(app.api.article.save))
 
 	app.route('/articles/:id')
 		.all(app.config.passport.authenticate())
 		.get(app.api.article.getById)
-		.put(app.api.article.save)
-		.delete(app.api.article.remove)
+		.put(admin(app.api.article.save))
+		.delete(admin(app.api.article.remove))
 
 	app.route('/categories/:id/articles')
 		.all(app.config.passport.authenticate())
